292eb00b4804558362ae6530404650e13acc7025
lumaldonado <lulimaldonado98@hotmail.com>
2021-12-21 12:25:14 -0300

    Se agrega el application properties

--- src/main/java/ar/com/fiera/linktracker/controllers/LinkController.java
+++ src/main/java/ar/com/fiera/linktracker/controllers/LinkController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.fiera.linktracker.entities.Link;
 import ar.com.fiera.linktracker.entities.Url;
+import ar.com.fiera.linktracker.entities.Link.EstadoLinkEnum;
 import ar.com.fiera.linktracker.models.request.GenericResponse;
 import ar.com.fiera.linktracker.models.request.InfoLinkNuevo;
 import ar.com.fiera.linktracker.services.LinkService;
@@ -21,31 +22,29 @@ public class LinkController {
     LinkService service;
 
     @PostMapping("/Link")
-    public ResponseEntity<?> crearLink(@RequestBody InfoLinkNuevo linkNuevo){
+    public ResponseEntity<?> crearLink(@RequestBody Link linkNuevo){
         
         GenericResponse respuesta = new GenericResponse();
-        Link link = service.crearLink(linkNuevo.linkId, linkNuevo.urlEnmascarado,linkNuevo.urlId,linkNuevo.estadoLink);
         respuesta.isOk = true;
-        respuesta.id = link.getLinkId();
+        respuesta.id = linkNuevo.getLinkId();
         respuesta.message = "El link fue creado con exito";
          return ResponseEntity.ok(linkNuevo);
 
-    
         
     }
     
 
     @PutMapping("/link/{id}")
-    public ResponseEntity<GenericResponse> invalidarLink(@PathVariable Integer linkId, @RequestBody EstadoLink estadoLink ){
+    public ResponseEntity<GenericResponse> invalidarLink(@PathVariable Integer linkId, @RequestBody EstadoLinkEnum estadoLink ){
         GenericResponse respuesta = new GenericResponse();
 
         Link link = service.buscarPorId(linkId);
-        link.setEstadoLinkId(estadoLink.estadoNuevo);
+        link.setEstadoLink(EstadoLinkEnum.INVALIDO);
         service.actualizar(link);
 
         respuesta.id = link.getLinkId();
         respuesta.isOk = true;
-        respuesta.message = "Estado del Link actualizado";
+        respuesta.message = "El link se ha invalidado";
 
         return ResponseEntity.ok(respuesta);
     }
