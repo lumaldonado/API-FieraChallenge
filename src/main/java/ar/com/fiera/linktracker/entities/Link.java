008c7ede16169fc2bc8dc1e2a0d1d4b966ef9a89
lumaldonado <lulimaldonado98@hotmail.com>
2021-12-21 11:16:11 -0300

    Se agregan los atributos de url y link, ademas de la relacion bidireccional y el enumerado de estadoLink

--- src/main/java/ar/com/fiera/linktracker/entities/Link.java
+++ src/main/java/ar/com/fiera/linktracker/entities/Link.java
@@ -1,10 +1,56 @@
 package ar.com.fiera.linktracker.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.util.Date;
+
+import javax.persistence.*;
+
 
 @Entity
 @Table(name = "link")
 public class Link {
     
+    @Column(name = "link_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int linkId;
+    
+    @Column(name = "fecha_expiracion")
+    private Date fechaExpiracion;
+
+    @Column(name = "url_enmascarado")
+    private String urlEnmascarado;
+
+    @ManyToOne
+    @JoinColumn(name = "url_id", referencedColumnName = "url_id")
+    private Url url;
+
+    @Column(name = "estado_id")
+    private EstadoLinkEnum estadoLink;
+
+
+    public enum EstadoLinkEnum {
+       VALIDO(1), INVALIDO(2);
+
+        private final int value;
+
+        private EstadoLinkEnum(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static EstadoLinkEnum parse(int id) {
+            EstadoLinkEnum status = null; // Default
+            for (EstadoLinkEnum item : EstadoLinkEnum.values()) {
+                if (item.getValue() == id) {
+                    status = item;
+                    break;
+                }
+            }
+            return status;
+        }
+    }
+
 }
--- src/main/java/ar/com/fiera/linktracker/entities/Url.java
+++ src/main/java/ar/com/fiera/linktracker/entities/Url.java
@@ -1,10 +1,23 @@
 package ar.com.fiera.linktracker.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import java.util.*;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "url")
 public class Url {
+
+    @Column(name = "url_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int urlId;
+
+    @Column(name = "url_entero")
+    private String urlEntero;
+
+    @OneToMany(mappedBy = "url", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Link> links = new ArrayList<>();
+
     
 }
